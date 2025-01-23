e2d10962519d7ca2cab692911412fa4bdb946abd
Fabri <ism.fabri@gmail.com>
2025-01-23 05:10:04 -0300

    add faker + update views + fix faker problem and +

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
     return render(request, 'pages/home.html', context={
-        'name': 'Fabri',
+        'recipes': [make_recipe() for _ in range(10)], # list_comprehension
     })
 
 def recipe(request, id):
     return render(request, 'pages/recipe-view.html', context={
-        'name': 'Fabri',
+        'recipe': make_recipe(),
     })
