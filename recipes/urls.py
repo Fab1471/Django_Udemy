397222a404de513f41dc85efc0c594ee5afad3bf
Fabri <ism.fabri@gmail.com>
2025-01-23 22:16:43 -0300

    dynamic urls + app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes' # com o app_name não é preciso passar o recipes no name da url no path
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
