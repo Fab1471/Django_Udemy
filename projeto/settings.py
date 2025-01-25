7fe2b7d5d925e722b4b4f21678df3cfdfc3a116e
Fabri <ism.fabri@gmail.com>
2025-01-25 16:28:54 -0300

    add imgs, categories

--- projeto/settings.py
+++ projeto/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/' # settando a url das mídias dos users
+MEDIA_ROOT = BASE_DIR / 'media' # settando a root das mídias dos users
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
